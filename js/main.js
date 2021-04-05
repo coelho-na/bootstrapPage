d4c2564e2ff264988d6f07f3460f673bdec61f1b
Nadine Coelho <nadinecoelho9@gmail.com>
2021-04-05 20:39:51 -0300

    adding function carousel to app section

--- js/main.js
+++ js/main.js
@@ -49,6 +49,23 @@ function toCaroussel() {
       },
     },
   });
+  $(".app-screenshots__carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    margin: 0,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 }
 
 // Nav Bar Shrink Function with Jquery
