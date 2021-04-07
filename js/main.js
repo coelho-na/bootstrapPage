66420abf6d070d53556a355254572aa2e1a148af
Nadine Coelho <nadinecoelho9@gmail.com>
2021-04-06 21:08:20 -0300

    finish html and add carousel to team section

--- js/main.js
+++ js/main.js
@@ -88,6 +88,26 @@ function toCaroussel() {
       },
     },
   });
+
+  // Team Carousel
+
+  $(".team__carousel").owlCarousel({
+    loop: true,
+    // autoplay: true,
+    margin: 0,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 }
 
 // Nav Bar Shrink Function with Jquery
